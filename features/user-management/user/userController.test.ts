e2833a11481682d05fcb1a420c6ff297f0f7ab63
Gaël Vanbeveren <gael.vanbeveren98@gmail.com>
2024-10-31 11:38:47 +0100

    test users routes ok

--- features/user-management/user/userController.test.ts
+++ features/user-management/user/userController.test.ts
@@ -2,7 +2,6 @@ import request from "supertest";
 import app from "../../../app"; // Ton fichier app.ts où tu configures Express
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import { generateToken } from "../../Auth/authController";
 import User, { IUser, matchPassword } from "../../../models/UserModel"; // Assurez-vous que le modèle User est bien typé
 import { MongoMemoryServer } from "mongodb-memory-server";
 
@@ -162,5 +161,121 @@ describe("PUT /users/updateName", () => {
 
     expect(res.statusCode).toBe(404);
     expect(res.body).toHaveProperty("message", "User not found");
+  });
+});
+
+describe("PUT /users/change-email", () => {
+  let userToken: string;
+  let user: IUser;
+
+  beforeEach(async () => {
+    // Créer un utilisateur de test et obtenir le token
+    const userResponse = await request(app).post("/auth/register").send({
+      name: "Test User",
+      email: "testuser@example.com",
+      password: "password123",
+    });
+
+    user = userResponse.body;
+    expect(user).toHaveProperty("token");
+    userToken = user.token;
+  });
+
+  it("should send verification email for changing email", async () => {
+    const res = await request(app)
+      .put("/users/change-email")
+      .set("Authorization", `Bearer ${userToken}`)
+      .send({
+        newEmail: "new-email@example.com",
+      });
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toHaveProperty(
+      "message",
+      "Verification email sent to new email address"
+    );
+
+    const updatedUser = await User.findOne({ email: "testuser@example.com" });
+    expect(updatedUser?.newEmail).toBe("new-email@example.com");
+    expect(updatedUser?.newEmailVerified).toBe(false);
+  });
+
+  it("should return 400 if email is already in use", async () => {
+    // Créer un autre utilisateur avec le même e-mail
+    await request(app).post("/auth/register").send({
+      name: "Another User",
+      email: "new-email@example.com",
+      password: "password123",
+    });
+
+    const res = await request(app)
+      .put("/users/change-email")
+      .set("Authorization", `Bearer ${userToken}`)
+      .send({
+        newEmail: "new-email@example.com",
+      });
+
+    expect(res.statusCode).toBe(400);
+    expect(res.body).toHaveProperty("message", "Email already in use");
+  });
+});
+
+describe("POST /users/verify-new-email", () => {
+  let userToken: string;
+  let user: any;
+  let newEmail: string;
+
+  beforeEach(async () => {
+    // Créez un utilisateur test et obtenez le token
+    const userResponse = await request(app).post("/auth/register").send({
+      name: "Test User",
+      email: "testuser@example.com",
+      password: "password123",
+    });
+
+    userToken = userResponse.body.token;
+
+    // Demandez la route 'changeEmail'
+    await request(app)
+      .put("/users/change-email")
+      .set("Authorization", `Bearer ${userToken}`)
+      .send({
+        newEmail: "new-email@example.com",
+      });
+
+    // Récupère l'utilisateur directement depuis la base de données pour accéder au mailToken
+    user = await User.findOne({ email: "testuser@example.com" });
+
+    newEmail = "new-email@example.com";
+  });
+
+  it("should verify new email with the mailToken", async () => {
+    // Exécute la requête avec le mailToken et l'email
+    const res = await request(app).post(
+      `/users/verify-new-email?token=${user.newEmailToken}&email=${newEmail}`
+    );
+
+    // Vérifie les réponses attendues
+    expect(res.status).toEqual(200);
+    expect(res.body).toHaveProperty(
+      "message",
+      "Email update confirmed successfully."
+    );
+
+    // Vérifie que l'utilisateur est bien marqué comme vérifié dans la base de données
+    const verifiedUser = await User.findOne({ email: "new-email@example.com" });
+    expect(verifiedUser?.newEmailVerified).toBe(true);
+    expect(verifiedUser?.newEmailToken).toBeNull(); // Vérifie que le token est supprimé
+  });
+
+  it("should throw an error if there is no mailtoken", async () => {
+    // Essayer de lancer la requête sans le mailToken
+    const res = await request(app).post(`/users/verify-new-email`).send({
+      email: newEmail,
+    });
+
+    // Vérifie les réponses attendues
+    expect(res.status).toEqual(400);
+    expect(res.body).toHaveProperty("message", "Token and email are required.");
   });
 });
